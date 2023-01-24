58de82cd01b8184b3e1be703500de6b079e274ba
LiamSmith1992 <liamj.smith@hotmail.com>
2023-01-24 15:49:33 -0700

    backend done

--- allSpice/Repositories/FavoritesRepository.cs
+++ allSpice/Repositories/FavoritesRepository.cs
@@ -27,6 +27,59 @@ SELECT LAST_INSERT_ID();
 
   }
 
+  internal List<MyFav> GetFavorites(string userId)
+  {
+    string sql = @"
+    SELECT
+    fav.*,
+    r.*,
+    a.*
+FROM favorites fav
+JOIN recipes r On r.id = fav.recipeId
+Join accounts a On r.creatorId = a.id
+WHERE fav.accountId = @userId;
+    ";
+    List<MyFav> myFav = _db.Query<Favorite, MyFav, Account, MyFav>(sql, (fav, r, a) =>
+    {
+      r.FavoriteId = fav.Id;
+      r.Creator = a;
+      return r;
+    }, new { userId }).ToList();
+    return myFav;
+  }
 
+  internal Favorite getOne(int id)
+  {
+    string sql = @"
+    SELECT * FROM favorites
+    WHERE id = @id;
+    ";
+    Favorite favorite = _db.Query<Favorite>(sql, new { id }).FirstOrDefault();
+    return favorite;
+    // r.*,
+    // a.*
+    // From favorites f
+    // JOIN recipe r ON f.RecipeId = r.Id
+    // JOIN accounts a On f.AccountId = a.Id
+    // WHERE f.id = @id
 
+    // Favorite favorite = _db.Query<Favorite, MyFav, Account, Favorite>(sql, (f, r, a) =>
+    // {
+    //   f.RecipeId = r.Id;
+    //   f.AccountId = a.Id;
+    //   return f;
+    // }, new { id }).FirstOrDefault();
+    // return favorite;
+
+  }
+
+  internal bool UnLike(int id)
+  {
+    string sql = @"
+   DELETE FROM favorites
+   WHERE id = @id;
+   ";
+    int rows = _db.Execute(sql, new { id });
+    return rows > 0;
+  }
 }
